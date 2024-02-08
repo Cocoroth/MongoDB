a41be3cd4abb19ac4ca27aed9988fbb5166b10de
Cocoroth <alejandro210599@gmail.com>
2024-02-08 08:58:28 +0100

    Esqueleto de la app

--- Menu.py
+++ Menu.py
@@ -1,1 +1,44 @@
+def main():
+    app = main()
+    while True:
+        print("1. Insertar documento")
+        print("2. Actualizar documento")
+        print("3. Reemplazar documento")
+        print("4. Eliminar documento")
+        print("5. Mostrar todos los documentos")
+        print("6. Búsqueda de documento")
+        print("7. Eliminar todos los documentos")
+        print("8. Salir")
 
+        choice = input("Seleccione una opción: ")
+
+        if choice == '1':
+            collection_name = input("Ingrese el nombre de la colección: ")
+            document = {}
+            app.insert_document(collection_name, document)
+        elif choice == '2':
+            # Implementar la actualización de documentos
+            pass
+        elif choice == '3':
+            # Implementar el reemplazo de documentos
+            pass
+        elif choice == '4':
+            # Implementar la eliminación de documentos
+            pass
+        elif choice == '5':
+            # Implementar la visualización de todos los documentos
+            pass
+        elif choice == '6':
+            # Implementar la búsqueda de documentos
+            pass
+        elif choice == '7':
+            # Implementar la eliminación de todos los documentos
+            pass
+        elif choice == '8':
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+from pymongo import MongoClient
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Main:
+    def __init__(self, host='localhost', port=27017, db_name='test'):
+        self.client = MongoClient(host, port)
+        self.db = self.client[db_name]
 
+    def insert_document(self, collection_name, document):
+        collection = self.db[collection_name]
+        return collection.insert_one(document)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def update_document(self, collection_name, query, new_values):
+        collection = self.db[collection_name]
+        return collection.update_many(query, {"$set": new_values})
 
+    def replace_document(self, collection_name, query, document):
+        collection = self.db[collection_name]
+        return collection.replace_one(query, document)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def delete_document(self, collection_name, query):
+        collection = self.db[collection_name]
+        return collection.delete_one(query)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def find_documents(self, collection_name, query={}):
+        collection = self.db[collection_name]
+        return collection.find(query)
+
+    def search_document(self, collection_name, query):
+        collection = self.db[collection_name]
+        return collection.find_one(query)
+
+    def delete_all_documents(self, collection_name):
+        collection = self.db[collection_name]
+        return collection.delete_many({})
+
+    def utility_operation1(self, collection_name):
+        pass
+
+    def utility_operation2(self, collection_name):
+        pass
